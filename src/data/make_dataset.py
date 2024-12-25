22e20f2628ebf0a0a6332cebc47f5d28da623151
Ouissam Gouni <g.ouissam@gmail.com>
2024-12-25 20:29:58 +0100

    init dvcdag

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import os
+from pathlib import Path
 
 df = pd.read_csv("data/raw_data/raw.csv")
 
@@ -8,6 +9,9 @@ target = df['silica_concentrate']
 feats = df.drop(['silica_concentrate','date'], axis=1)
 X_train, X_test, y_train, y_test = train_test_split(feats, target, test_size=0.3, random_state=42)
 
-for file, filename in zip([X_train, X_test, y_train, y_test], ['X_train', 'X_test', 'y_train', 'y_test']):
-    output_filepath = os.path.join("data/processed_data", f'{filename}.csv')
-    file.to_csv(output_filepath, index=False)
+output_dir = Path("data/processed_data")
+output_dir.mkdir(parents=True, exist_ok=True)
+
+for data, filename in zip([X_train, X_test, y_train, y_test], ['X_train', 'X_test', 'y_train', 'y_test']):
+    output_filepath = output_dir.joinpath(f'{filename}.csv')
+    data.to_csv(output_filepath, index=False)
